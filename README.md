25072293469dc431e676c049d17c5b36ba3e5a9b
Raye <astingraye@proton.me>
2025-06-15 23:01:59 +0100

    update readme and logging

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wokehide
 A semi-satirical cli tool to loosely "hide" images using base64 encoding/decoding
+
+## Building
+```sh
+sudo rm "$(which wokehide)"; go build -o wokehide ./src; sudo mv ./wokehide /usr/bin
+```
--- src/main.go
+++ src/main.go
@@ -17,15 +17,15 @@ func main() {
 
 	arg.MustParse(&a)
 
-	fmt.Println(a.Positional)
-
 	if IsBase64(a.Positional) {
 		exec.Command("xdg-open", DecodeBase64(a.Positional)).Start()
+		fmt.Println("Acquired the woke")
 	} else {
 		if url, err := catbox.New(nil).Upload(a.Positional); err != nil {
 			fmt.Println(err)
 		} else {
 			exec.Command("wl-copy", EncodeBase64(url)).Start()
+			fmt.Println("Successfully encoded the woke")
 		}
 	}
 }
